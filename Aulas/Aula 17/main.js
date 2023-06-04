5637c57c1d43a2fd85d9309aef7106db2bdf04fa
Hiago Silveira <silveira.hiago@gmail.com>
2023-06-03 23:09:05 -0300

    atz

---
+++ Aulas/Aula 17/main.js
@@ -0,0 +1,27 @@
+
+//const meuNome = "Hiago Silveira"
+
+//const numero1 = 5 
+//const numero2 = 7
+
+//let soma
+
+//soma = numero1 + numero2
+//multiplicacao = numero1 * numero2
+
+//alert("Resultado da soma: " + soma)
+//console.log(multiplicacao)
+
+function subtracao(numero1, numero2){
+    if(numero1 > numero2){
+        let resultado = numero1 - numero2
+        console.log(numero1 + "-" + numero2 + "=" + resultado)
+    }
+    else{
+        console.log("Não foi possível subtrair" + numero + "de" + numero1)
+    }
+
+}
+
+subtracao (10,7)
+subtracao (4,7)
